f0d8759e49c941e1c4c98b78621bbc5ee5537ca9
Christopher <CHuber42.work@gmail.com>
2020-06-05 15:24:27 -0700

    add browse for non-logged-in-users

--- PSST/Controllers/HomeController.cs
+++ PSST/Controllers/HomeController.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using SweetSavory.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Linq;
 
 namespace SweetSavory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly SweetSavoryContext _db;
+
+    public HomeController(SweetSavoryContext db)
+    {
+      _db = db;
+    }
     public ActionResult Index()
     {
       return View("Index");
+    }
+
+    public ActionResult Flavors()
+    {
+      List<Flavor> allFlavors = _db.Flavors.ToList();
+      return View("BrowseFlavors", allFlavors);
+    }
+
+    public ActionResult Treats()
+    {
+      List<Treat> allTreats = _db.Treats.ToList();
+      return View("BrowseTreats", allTreats);
     }
   }
 }
